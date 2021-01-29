d60f9e43704c73df3ebdb5373c9472c5b225ac86
TimT3032 <72447992+TimT3032@users.noreply.github.com>
2021-01-29 08:25:49 -0500

    Add files via upload

--- ground.js
+++ ground.js
@@ -1,18 +1,17 @@
-class GROUND{
-    constructor(x,y,width,height){
-        var options = {
-            'isStatic':true
-        }
-        this.body = Bodies.rectangle(x,y,width,height,options);
-        this.width = width;
-        this.height = height;
-
-        World.add(world,this.body);
+class Ground{
+    constructor(x,y,w,h) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,w,h,options);
+      this.w = w; 
+      this.h = h;
+      World.add(world, this.body);
     }
     display(){
-        var pos = this.body.position;
-        rectMode(CENTER);
-        fill("brown");
-        rect(pos.x,pos.y,this.width,this.height);
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("limegreen");
+    rect(pos.x, pos.y, this.w,this.h);
     }
-}
+  };
